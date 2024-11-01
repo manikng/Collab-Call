609f590450c93cf1a97da1b3fc3c221ea1ce0e82
manikng <154789263+manikng@users.noreply.github.com>
2024-11-01 18:05:09 +0000

    jsx can only return one tag wrapper only

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 
 export default function App() {
-  return (
+  return (<>
     <h1 className="text-3xl text-center font-bold underline">
       Hello world!
     </h1>
+    <h3 className="bg-blue-950 text-5xl fond bold text-white">hello</h3>
+    </>
   )
 }
